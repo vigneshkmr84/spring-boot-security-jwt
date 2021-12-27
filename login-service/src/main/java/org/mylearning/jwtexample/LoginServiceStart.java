4441cd1f2b111e41baea4268b26543ea6ef2ad01
Vignesh <vigneshkmr84@gmail.com>
2021-12-27 14:00:03 -0600

    Separated Login module - that  connects purely to login database / SSO / LDAP or other authentication & authorization mechanisms

---
+++ login-service/src/main/java/org/mylearning/jwtexample/LoginServiceStart.java
@@ -0,0 +1,13 @@
+package org.mylearning.jwtexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginServiceStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginServiceStart.class, args);
+    }
+
+}
